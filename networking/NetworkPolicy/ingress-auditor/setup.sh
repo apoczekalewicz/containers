74b4f5aa3b39184e4e27c8e187ba06e175f1e211
ap <ap@laptop.ap.local>
2021-09-24 15:45:03 +0200

    commit

--- networking/NetworkPolicy/ingress-auditor/setup.sh
+++ networking/NetworkPolicy/ingress-auditor/setup.sh
@@ -2,5 +2,5 @@ oc new-project net-audit
 oc create sa net-audit-sa
 # oc apply -f 01_ClusterRole_net-audit.yml
 # oc adm policy add-cluster-role-to-user net-audit-role -z net-audit-sa -n net-audit
-# export TOKEN=$( oc extract secret/`oc get sa/net-audit-sa -n net-audit -o jsonpath='{.secrets[0].name}{"\n"}'` --keys=token --to=- 2>/dev/null )
+# export TOKEN=$(oc extract secret/`oc get sa/net-audit-sa -n net-audit -o jsonpath='{.secrets[*].name}{"\n"}' | tr ' ' '\n' | grep token` --keys=token --to=- 2>/dev/null)
 # export ENDPOINT=$(oc config current-context | cut -d/ -f2 | tr - .)
