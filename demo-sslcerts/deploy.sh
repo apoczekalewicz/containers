b057303b879807521d3918d6ad883cc2aab92317
ap <ap@localhost>
2021-10-09 19:32:10 +0200

    commit

--- demo-sslcerts/deploy.sh
+++ demo-sslcerts/deploy.sh
@@ -10,7 +10,7 @@ oc project acm-grc-ansible-example
 oc -n acm-grc-ansible-example create secret generic certs --from-file=tls.key=/tmp/tls/tls.key --from-file=tls.crt=/tmp/tls/tls.crt
 
 # app
-oc -n acm-grc-ansible-example apply -f app.yml
+oc -n acm-grc-ansible-example apply -f k8s/app.yml
 
 export ROUTE=$(oc -n acm-grc-ansible-example get route | tail -1 | tr -s ' ' | cut -f 2 -d ' ')
 echo $ROUTE
