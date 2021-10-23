9dbc54b92dc832555950b505bd46737689042aac
ap <ap@localhost>
2021-10-23 16:16:24 +0200

    commit

--- demossl/create_secret.sh
+++ demossl/create_secret.sh
@@ -1,5 +1,5 @@
 mkdir -p /tmp/tls
 openssl req -new -newkey rsa:4096 -days 25 -nodes -x509 -subj "/C=US/ST=NC/L=Raleigh/O=Example/CN=www.example.com" -keyout /tmp/tls/tls.key -out /tmp/tls/tls.crt
 
-oc create secret generic certs --from-file=tls.key=/tmp/tls/tls.key --from-file=tls.crt=/tmp/tls/tls.crt -o yaml --dry-run=client  > k8s/secret.yml
+oc create secret generic certs --from-file=tls.key=/tmp/tls/tls.key --from-file=tls.crt=/tmp/tls/tls.crt -o yaml --dry-run=client  > manifests/secret.yml
 
