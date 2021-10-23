56f31a65110594f2b325b14ead380b5fe58773be
ap <ap@localhost>
2021-10-23 16:22:14 +0200

    commit

--- demossl/create_secret.sh
+++ demossl/create_secret.sh
@@ -3,3 +3,4 @@ openssl req -new -newkey rsa:4096 -days 25 -nodes -x509 -subj "/C=US/ST=NC/L=Ral
 
 oc create secret generic certs --from-file=tls.key=/tmp/tls/tls.key --from-file=tls.crt=/tmp/tls/tls.crt -o yaml --dry-run=client  > manifests/secret.yml
 
+#openssl s_client -connect demossl-ansibletest.apps.ocp1.example.lab:443  | openssl x509 -text|less
