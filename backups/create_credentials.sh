c3b0013b4de13512e5987442dbfcfca643482cff
ap <ap@localhost>
2021-11-19 01:54:52 +0100

    commit

--- backups/create_credentials.sh
+++ backups/create_credentials.sh
@@ -1,7 +1,7 @@
 cat > noobaa-credentials <<EOF
 [default]
-aws_access_key_id=$NOOBAA_S3_ACCESS_KEY_ID
-aws_secret_access_key=$NOOBAA_S3_SECRET_ACCESS_KEY
+aws_access_key_id=HqJiTngJEsL6oNSB8MAC
+aws_secret_access_key=qpsr0XYrWTvdATHOOA/6QDlWC8MFcGx9BZDKWYbX
 EOF
 
 oc create secret generic cloud-credentials --namespace openshift-adp --from-file cloud=noobaa-credentials --dry-run -o yaml --save-config
