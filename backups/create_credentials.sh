4352667ce379b52fccf330f5f7667631431f6323
ap <ap@localhost>
2021-11-19 03:10:14 +0100

    commit

--- backups/create_credentials.sh
+++ backups/create_credentials.sh
@@ -4,4 +4,6 @@ aws_access_key_id=HqJiTngJEsL6oNSB8MAC
 aws_secret_access_key=qpsr0XYrWTvdATHOOA/6QDlWC8MFcGx9BZDKWYbX
 EOF
 
-oc create secret generic cloud-credentials --namespace openshift-adp --from-file cloud=noobaa-credentials --dry-run -o yaml --save-config
+oc create secret generic cloud-credentials --namespace openshift-adp --from-file cloud=noobaa-credentials --dry-run=client -o yaml --save-config | oc apply -f - 
+
+rm -f noobaa-credentials
