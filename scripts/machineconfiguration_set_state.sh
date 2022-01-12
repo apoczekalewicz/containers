e98f56920c04238c1206b09c0eea7ea002e75b59
ap <ap@localhost>
2022-01-12 18:10:34 +0100

    commit

--- scripts/machineconfiguration_set_state.sh
+++ scripts/machineconfiguration_set_state.sh
@@ -7,7 +7,7 @@ then
 fi
 node_name=$1
 new_value=$2
-oc patch node $node_name --type merge --patch '{"metadata": {"annotations": {"machineconfiguration.openshift.io/currentConfig": "$new_value"}}}' 
-oc patch node $node_name  --type merge --patch '{"metadata": {"annotations": {"machineconfiguration.openshift.io/desiredConfig": "$new_value"}}}' 
-oc patch node $node_name  --type merge --patch '{"metadata": {"annotations": {"machineconfiguration.openshift.io/reason": ""}}}' 
-oc patch node $node_name  --type merge --patch '{"metadata": {"annotations": {"machineconfiguration.openshift.io/state": "Done"}}}'
+echo oc patch node $node_name --type merge --patch "'{\"metadata\": {\"annotations\": {\"machineconfiguration.openshift.io/currentConfig\": \"$new_value\"}}}'" | bash
+echo oc patch node $node_name --type merge --patch "'{\"metadata\": {\"annotations\": {\"machineconfiguration.openshift.io/desiredConfig\": \"$new_value\"}}}'" | bash
+echo oc patch node $node_name --type merge --patch "'{\"metadata\": {\"annotations\": {\"machineconfiguration.openshift.io/reason\": \"\"}}}'" | bash
+echo oc patch node $node_name --type merge --patch "'{\"metadata\": {\"annotations\": {\"machineconfiguration.openshift.io/state\": \"Done\"}}}'" | bash
