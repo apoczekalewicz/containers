02b9b9e909cbeda805fe0fac8bff00e275a84ceb
ap <ap@localhost>
2022-01-13 10:00:01 +0100

    commit

--- scripts/ocs.sh
+++ scripts/ocs.sh
@@ -1,2 +1,3 @@
+oc patch OCSInitialization ocsinit -n openshift-storage --type json --patch  '[{ "op": "replace", "path": "/spec/enableCephTools", "value": true }]'
 TOOLS_POD=$(oc get pods -n openshift-storage -l app=rook-ceph-tools -o name)
 oc rsh -n openshift-storage $TOOLS_POD
