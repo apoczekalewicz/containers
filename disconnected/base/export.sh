ade266f479d4e9952e4693820398fed821bee08d
ap <ap@localhost>
2022-02-17 01:19:23 +0100

    commit

---
+++ disconnected/base/export.sh
@@ -0,0 +1,8 @@
+export LOCAL_REGISTRY=localhost:8443
+export LOCAL_REPOSITORY="ocp4/openshift4"
+export PRODUCT_REPO='openshift-release-dev'
+export OCP_RELEASE="4.9.5"
+export LOCAL_SECRET_JSON=./pull-secret.txt
+export RELEASE_NAME="ocp-release"
+export ARCHITECTURE="x86_64"
+export REMOVABLE_MEDIA_PATH="./katalog"
---
+++ disconnected/base/to_dir.sh
@@ -0,0 +1,1 @@
+oc adm release mirror -a ${LOCAL_SECRET_JSON} --to-dir=${REMOVABLE_MEDIA_PATH}/mirror quay.io/${PRODUCT_REPO}/${RELEASE_NAME}:${OCP_RELEASE}-${ARCHITECTURE}
