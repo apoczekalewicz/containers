67e06a8a7e0f92fc0f1aa7a0798d23cc1124d097
ap <ap@localhost>
2022-03-19 17:55:33 +0100

    commit

--- disconnected/base/from_dir.sh
+++ disconnected/base/from_dir.sh
@@ -1,1 +1,1 @@
-oc image mirror -a ${LOCAL_SECRET_JSON} --from-dir=${REMOVABLE_MEDIA_PATH}/mirror "file://openshift/release:${OCP_RELEASE}-${ARCHITECTURE}*" ${LOCAL_REGISTRY}/${LOCAL_REPOSITORY} 
+oc image mirror -a ${LOCAL_SECRET_JSON} --from-dir=${REMOVABLE_MEDIA_PATH}/mirror "file://openshift/release:${OCP_RELEASE}-${ARCHITECTURE}*" --to=${LOCAL_REGISTRY}/${LOCAL_REPOSITORY} --to-release-image=${LOCAL_REGISTRY}/${LOCAL_REPOSITORY}/${RELEASE_NAME}:${OCP_RELEASE}-${ARCHITECTURE}
--- disconnected/base/review.sh
+++ disconnected/base/review.sh
@@ -1,4 +1,2 @@
-oc adm release mirror -a ${LOCAL_SECRET_JSON}  \
-     --from=quay.io/${PRODUCT_REPO}/${RELEASE_NAME}:${OCP_RELEASE}-${ARCHITECTURE} \
-     --to=${LOCAL_REGISTRY}/${LOCAL_REPOSITORY} \
-     --to-release-image=${LOCAL_REGISTRY}/${LOCAL_REPOSITORY}:${OCP_RELEASE}-${ARCHITECTURE} --dry-run
+oc adm release mirror -a ${LOCAL_SECRET_JSON} --from=quay.io/${PRODUCT_REPO}/${RELEASE_NAME}:${OCP_RELEASE}-${ARCHITECTURE} --to=${LOCAL_REGISTRY}/${LOCAL_REPOSITORY} --to-release-image=${LOCAL_REGISTRY}/${LOCAL_REPOSITORY}/${RELEASE_NAME}:${OCP_RELEASE}-${ARCHITECTURE} --dry-run
+# --apply-release-image-signature
