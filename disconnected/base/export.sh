04261848abd3cb564712af29ee6ad7c4d00b44c0
ap <ap@localhost>
2022-03-20 01:55:01 +0100

    commit

--- disconnected/base/export.sh
+++ disconnected/base/export.sh
@@ -1,7 +1,7 @@
 export LOCAL_REGISTRY=work.ap.vlab:8443
 export LOCAL_REPOSITORY="ocp4/openshift4"
 export PRODUCT_REPO='openshift-release-dev'
-export OCP_RELEASE="4.9.23"
+export OCP_RELEASE="4.9.17"
 export LOCAL_SECRET_JSON=./pull-secret.txt
 export RELEASE_NAME="ocp-release"
 export ARCHITECTURE="x86_64"
