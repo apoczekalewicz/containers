0bbb17757a52d93127cfebe19594a2e6e37cf09b
ap <ap@localhost>
2022-05-25 14:18:37 +0200

    commit

--- scripts/storage/ODF/device_replacing.sh
+++ scripts/storage/ODF/device_replacing.sh
@@ -10,6 +10,6 @@ osd_id_to_remove=$1
 oc scale -n openshift-storage deployment rook-ceph-osd-${osd_id_to_remove} --replicas=0
 oc delete -n openshift-storage job ocs-osd-removal-job
 oc process -n openshift-storage ocs-osd-removal -p FAILED_OSD_IDS=${osd_id_to_remove} |oc create -n openshift-storage -f -
-oc wait --for=condition=Complete job ocs-osd-removal-jo
+oc wait --for=condition=Complete job ocs-osd-removal-job
 oc delete -n openshift-storage job ocs-osd-removal-job
 watch -n0 "oc get deployment rook-ceph-osd-${osd_id_to_remove}"
