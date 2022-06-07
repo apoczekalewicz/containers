9e690e625b043ead71f9f2adc8fee35452c4da0d
ap <ap@localhost>
2022-06-07 18:39:59 +0200

    commit

--- scripts/CRD/crd_objects.sh
+++ scripts/CRD/crd_objects.sh
@@ -13,8 +13,8 @@ for CRD in $(oc get crd  | grep $FILTER | awk '{ print $1 }')
 do
 		if [[ "$ACTION" = "delete" ]]
 		then
-			oc delete $CRD --all -A
-			oc delete crd $CRD
+			oc delete $CRD --all -A --cascade=orphan --force &
+			oc delete crd $CRD --cascade=orphan --force &
 		else
 			echo -e "CRD: $CRD"
 		fi
