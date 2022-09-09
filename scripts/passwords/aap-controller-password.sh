0260646bd7de4f7eee669b499eef1a01398ea345
ap <ap@localhost>
2022-09-09 22:13:07 +0200

    commit

--- scripts/passwords/aap-controller-password.sh
+++ scripts/passwords/aap-controller-password.sh
@@ -1,1 +1,7 @@
+echo AAP:
 oc -n ansible-automation-platform get secret controller-admin-password -o go-template='{{index .data "password" | base64decode}}'
+echo ""
+echo AutomationHub:
+oc -n ansible-automation-platform get secret automationhub-admin-password -o go-template='{{index .data "password" | base64decode}}'
+echo ""
+
