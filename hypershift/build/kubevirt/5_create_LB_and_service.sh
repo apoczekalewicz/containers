0ea5d046de3c31e89dcf1276f64ecfe17e0955e4
Artur <artur@poczekalewicz.net>
2022-09-25 13:21:04 +0200

    commit

--- hypershift/build/kubevirt/5_create_LB_and_service.sh
+++ hypershift/build/kubevirt/5_create_LB_and_service.sh
@@ -35,7 +35,7 @@ metadata:
   name: ${KUBEVIRT_CLUSTER_NAME}-443
   namespace: ${KUBEVIRT_CLUSTER_NAMESPACE}
 spec:
-  host: wildcard.apps.${KUBEVIRT_CLUSTER_BASE_DOMAIN}
+  host: apps.${KUBEVIRT_CLUSTER_BASE_DOMAIN}
   wildcardPolicy: Subdomain
   tls:
     termination: passthrough
--- hypershift/build/kubevirt/export.sh
+++ hypershift/build/kubevirt/export.sh
@@ -1,5 +1,5 @@
 export KUBEVIRT_CLUSTER_NAME=nested
 export KUBEVIRT_CLUSTER_NAMESPACE=clusters-${KUBEVIRT_CLUSTER_NAME}
-export KUBEVIRT_CLUSTER_BASE_DOMAIN=${KUBEVIRT_CLUSTER_NAME}.apps.zpa.redhat.lab
+export KUBEVIRT_CLUSTER_BASE_DOMAIN=apps.zpa.redhat.lab
 export PULL_SECRET="$HOME/pull-secret"
 
