3667d0224de71cc3dacfbcb95841eeca28e45631
Artur <artur@poczekalewicz.net>
2023-02-12 03:28:24 +0100

    commit

--- scripts/storage/pvc_explorer.sh
+++ scripts/storage/pvc_explorer.sh
@@ -3,6 +3,18 @@
 [[ "$#" -ne "1" ]] && echo "Usage: $0 pvcname" && exit 1
 
 PVC=${1}
-echo "Your PVC: ${PVC} should be on /pvc"
-oc run -it --rm pvcexplorer --image=dummy --restart=Never --overrides='{"spec":{"containers":[{"restartPolicy":"Never", "tty":true, "stdin": true, "command":["/bin/sh", "-c", "bash"],"image":"quay.io/apoczeka/container-toolbox","name":"tmp-pod","volumeMounts":[{"mountPath":"/pvc","name":"volume"}]}],"volumes":[{"name":"volume","persistentVolumeClaim":{"claimName":"'${PVC}'"}}]}}'
 
+MODE=$( oc get pvc $PVC -o yaml | grep volumeMode | awk '{ print $2 }' )
+
+if [[ "$MODE" == "Filesystem" ]]
+then
+
+	echo "Your PVC: ${PVC} should be on /pvc"
+	oc run -it --rm pvcexplorer --image=dummy --restart=Never --overrides='{"spec":{"containers":[{"restartPolicy":"Never", "tty":true, "stdin": true, "command":["/bin/sh", "-c", "bash"],"image":"quay.io/apoczeka/container-toolbox","name":"tmp-pod","volumeMounts":[{"mountPath":"/pvc","name":"volume"}]}],"volumes":[{"name":"volume","persistentVolumeClaim":{"claimName":"'${PVC}'"}}]}}' 2>/dev/null
+
+else
+
+	echo "Your PVC: ${PVC} should be on /dev/pvc"
+	oc run -it --rm pvcexplorer --image=dummy --restart=Never --overrides='{"spec":{"containers":[{"restartPolicy":"Never", "tty":true, "stdin": true, "command":["/bin/sh", "-c", "bash"],"image":"quay.io/apoczeka/container-toolbox","name":"tmp-pod","volumeDevices":[{"devicePath":"/dev/pvc","name":"volume"}]}],"volumes":[{"name":"volume","persistentVolumeClaim":{"claimName":"'${PVC}'"}}]}}' 2>/dev/null
+
+fi
