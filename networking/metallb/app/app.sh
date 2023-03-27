18230d3f94cda1df6c1e148c6668a0059c414351
ap <ap@localhost>
2023-03-27 18:22:24 +0200

    commit

--- networking/metallb/app/app.sh
+++ networking/metallb/app/app.sh
@@ -1,4 +1,5 @@
-oc new-app https://github.com/apoczekalewicz/example-source.git
-oc expose svc example-source
+oc new-project example1
+oc new-app https://github.com/apoczekalewicz/app-info.git --context-dir ./src
+oc expose svc app-info
 oc get route
 
