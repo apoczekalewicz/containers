27d6f1f870f32aeda1b95fcc0edf1f113900597c
ap <ap@localhost>
2023-03-27 15:43:47 +0200

    commit

--- networking/NetworkPolicy/ingress/setup.sh
+++ networking/NetworkPolicy/ingress/setup.sh
@@ -3,8 +3,11 @@ oc new-project demo-net2
 oc project demo-net1
 oc apply -f https://raw.githubusercontent.com/apoczekalewicz/containers/master/httpd-deployments/httpd-deployment/deployment.yml
 oc apply -f https://raw.githubusercontent.com/apoczekalewicz/containers/master/httpd-deployments/httpd-deployment/svc.yaml
+oc apply -f https://raw.githubusercontent.com/apoczekalewicz/containers/master/httpd-deployments/httpd-deployment/cm-index.yaml
 oc project demo-net2
 oc apply -f https://raw.githubusercontent.com/apoczekalewicz/containers/master/httpd-deployments/httpd-deployment/deployment.yml
 oc apply -f https://raw.githubusercontent.com/apoczekalewicz/containers/master/httpd-deployments/httpd-deployment/svc.yaml
+oc apply -f https://raw.githubusercontent.com/apoczekalewicz/containers/master/httpd-deployments/httpd-deployment/cm-index.yaml
+# oc label namespace net=trusted
+# oc -n demo-net1 exec -it httpd-demo-86cbbb9d55-k9tsc --  curl http://httpd-demo.demo-net2
 
-# oc label project/demo-net2 project=demo-net2
