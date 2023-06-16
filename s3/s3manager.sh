5a74b45a7e272a47369f95bd806b0e9a22b8bc0c
Artur <artur@poczekalewicz.net>
2023-06-16 17:53:06 +0200

    commit

--- s3/s3manager.sh
+++ s3/s3manager.sh
@@ -1,3 +1,17 @@
-oc new-app mastertinner/s3manager -e ENDPOINT=s3-openshift-storage.apps.ocp1.example.lab -e ACCESS_KEY_ID="KzfRX3OcSzyd7ln3eXx5" -e SECRET_ACCESS_KEY="3arCAlT0EY/wdK54WgC+/WasxVd5T1aYzfb44ixd" -e SKIP_SSL_VERIFICATION=true
+if [[ "$#" -ne 2 ]]
+then
+	echo "Usage: $0 <ns> <secret>"
+	exit 1
+fi
+
+NS=$1
+SECRET=$2
+ENDPOINT=s3.openshift-storage.svc
+
+ACCESS_KEY_ID=$( oc get secret -o yaml -n $NS $SECRET | grep AWS_ACCESS_KEY_ID | awk '{ print $2 }' | base64 -d )
+SECRET_ACCESS_KEY=$( oc get secret -o yaml -n $NS $SECRET | grep AWS_SECRET_ACCESS_KEY | awk '{ print $2 }' | base64 -d )
+
+oc new-app cloudlena/s3manager -e ENDPOINT=$ENDPOINT -e ACCESS_KEY_ID=$ACCESS_KEY_ID -e SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY -e SKIP_SSL_VERIFICATION=true
+oc expose svc/s3manager
 
 
